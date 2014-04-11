d7705326efdef3faa9d1c8153d04909ae260d477
soojin.ro <sugarpoint33@gmail.com>
2014-04-11 11:29:29 +0900

    GCM

--- app/src/main/java/com/example/angtalk/app/MakeRequest.java
+++ app/src/main/java/com/example/angtalk/app/MakeRequest.java
@@ -25,6 +25,9 @@ import java.util.Map;
  */
 public class MakeRequest extends AsyncTask<String, String, String> {
 
+    public static final String TAG = "AngTok";
+    public static final String SERVER_URI = "http://10.73.45.162:5000/";
+
     @Override
     protected String doInBackground(String... strings) {
         DefaultHttpClient httpClient = new DefaultHttpClient();
@@ -32,7 +35,7 @@ public class MakeRequest extends AsyncTask<String, String, String> {
         URI uri = null;
 
         try {
-            uri = new URI("http://10.73.45.162:5000/");
+            uri = new URI(SERVER_URI);
 
             HttpPost httpPost = new HttpPost(uri);
 
@@ -41,24 +44,26 @@ public class MakeRequest extends AsyncTask<String, String, String> {
             se.setContentType("application/json;charset=UTF-8");
             se.setContentEncoding(new BasicHeader(HTTP.CONTENT_TYPE, "application/json;charset=UTF-8"));
 
+            Log.i(TAG, "StringEntity: " + se.toString());
             httpPost.setHeader("Accept", "application/json");
             httpPost.setHeader("Content-type", "application/json");
             httpPost.setEntity(se);
             //ResponseHandler responseHandler = new BasicResponseHandler();
 
-
             HttpResponse httpResponse = httpClient.execute(httpPost);
 
             InputStreamReader isr = new InputStreamReader(httpResponse.getEntity().getContent(), "UTF-8");
 
-            String s = null;
+            String s = "";
             s += isr.read();
-            Log.e("DDD", s);
+            Log.i(TAG, "HTTP CONTENT: " + s);
+            Log.i(TAG, "HTTP CODE: " + httpResponse.getStatusLine().getStatusCode() + " \n"
+                    + "Phrase: " + httpResponse.getStatusLine().getReasonPhrase());
 
             return s;
         }
         catch (Exception e) {
-            Log.e("ERR : ", e.toString());
+            Log.e(TAG, e.toString());
         }
 
         return null;
